faf80f986cc76ee3f5422f3d90845751b3f8179b
jallal-msd <jallalblack@gmail.com>
2023-01-28 20:01:38 +0100

    antonio

--- src/App.svelte
+++ src/App.svelte
@@ -3,12 +3,12 @@
 
     
 </script>
-<div class="container mx-auto p-5"> 
-    <h1 class="text-center-2xl">Try to guess erobb221's emote </h1>
-    <p class="">LOW EFFORT GAME, <img class=" "src="images/4finger.webp" alt="">CLM </p>
+<!--<div class="container mx-auto p-5"> -->
+    <!--<h1 class="text-center-2xl">Try to guess erobb221's emote </h1>-->
+    <!--<p class="">LOW EFFORT GAME, <img class=" "src="images/4finger.webp" alt="">CLM </p>-->
     
-</div>
-    <Trv/>
-
+<!--</div>-->
+    <!--<Trv/>-->
+<h1 class="text-x">I m antonio  </h1>
 <style>
 </style>
